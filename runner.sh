6a6f9a62a69e72917b03f895d863ab45858f4ab9
Shuja Khalid <shujakhalid@gmail.com>
2022-07-04 11:12:02 -0400

    ACTUALLY STARTING TO SEE SOME RESULTS... Added VOCSegmentation as well

--- runner.sh
+++ runner.sh
@@ -2,5 +2,12 @@
 
 #DATASET_PATH='/home/skhalid/Documents/datalake/shakespeare.txt'
 # DATASET_PATH='/home/skhalid/Documents/datalake/cifar-10-python/cifar-10-batches-py'
-rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/1.json"
+#rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/1.json"
 
+jobs=$(ls -lrt ./jobs/vector/ | awk '{print $9}')
+jobs_dir=./jobs/vector/
+for job in ${jobs[@]}
+do
+    echo $job
+    sbatch ./_launch.sh $jobs_dir$job
+done
