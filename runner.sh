eaa827ae2df8e4d84d2f816be68d52c3fbc3df69
Shuja Khalid <shujakhalid@gmail.com>
2022-07-07 04:10:42 -0400

    sending over new jobs

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 
 #DATASET_PATH='/home/skhalid/Documents/datalake/shakespeare.txt'
 # DATASET_PATH='/home/skhalid/Documents/datalake/cifar-10-python/cifar-10-batches-py'
-rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/1.json"
+rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/base.json"
 
 # jobs=$(ls -lrt ./jobs/vector/*.json | awk '{print $9}')
 # jobs_dir=./jobs/vector/
