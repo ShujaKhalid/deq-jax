9d6d29b6e0b21415fb1a01ef12181753841fab2d
Shuja Khalid <shujakhalid@gmail.com>
2022-07-07 04:20:20 -0400

    sending over new jobs

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ class CheckpointingUpdater:
 def main(config):
 
     config["checkpoint_dir"] = config["logging"]["logdir"] + \
-        str(int(time.time())) + "/"
+        str(int(time.time_ns()/1000)) + "/"
 
     # Save a snapshot of the code to the checkpoint directory
     os.system("mkdir -p "+config["checkpoint_dir"])
