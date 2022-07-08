12bcec3bf633d5b9cf597b4c97ebfde6117bad26
Shuja Khalid <shujakhalid@gmail.com>
2022-07-08 07:16:58 -0400

    Fixed Jaccard AGAIN...

--- utils/metrics.py
+++ utils/metrics.py
@@ -18,24 +18,35 @@ def jaccard(params, rng, x_patch, x, y, ver, save_img_to_folder, forward_fn, con
     if (config["data_attrs"]["num_classes"] != 1):
         print("y.unique(): {}".format(np.unique(y)))
 
+        dims = y.shape[1] * y.shape[2]
         logits = jax.nn.softmax(logits, axis=-1)
-        y = jax.nn.one_hot(y, config["data_attrs"]["num_classes"])
-        print("ver: {} - y: {} - y_hat: {}".format(ver, y.shape, logits.shape))
+        y_ohe = jax.nn.one_hot(y, config["data_attrs"]["num_classes"])
+        print("ver: {} - y: {} - y_hat: {} - y_ohe: {}".format(ver,
+              y.shape, logits.shape, y_ohe.shape))
 
-        y_rsp = y.reshape(-1, y.shape[-1]).astype(bool)
+        y_rsp = y_ohe.reshape(-1, y_ohe.shape[-1]).astype(bool)
         logits_rsp = logits.reshape(-1, logits.shape[-1]).astype(bool)
         print("ver: {} - y_rsp: {} - logits_rsp: {}".format(ver,
               y_rsp.shape, logits_rsp.shape))
 
         # prep
+        # y_rsp = y_rsp != 0.0
+        # logits_rsp = logits_rsp != 0.0
         n = np.bitwise_and(y_rsp, logits_rsp)
         u = np.bitwise_or(y_rsp, logits_rsp)
         jaccard_matrix = (n/u).astype(bool)
-        jaccard_matrix[jaccard_matrix != jaccard_matrix] = 0.0
+        # jaccard_matrix[jaccard_matrix != jaccard_matrix] = 0.0
+        class_img_map = {cls: np.sum([1.0 if cls in y[b, :, :] else 0.0 for b in range(y.shape[0])]) for cls in range(
+            config["data_attrs"]["num_classes"])}
+        print("class_exist: {}".format(class_img_map))
+        print("class_exist: {}".format(class_img_map[0]))
+        print("class_img_map: {}".format(type(class_img_map[0])))
+        print("class_img_map: {}".format(np.sum(class_img_map[0])))
+        print("jaccard_matrix: {}".format(np.sum(jaccard_matrix[:, 0])))
         # print("sum_check: {}".format(jnp.sum(logits[0, 0, 0, :])))
 
         # Jaccard class-wise
-        jaccard_classwise = {v: np.sum(jaccard_matrix[:, v])/(y_rsp.shape[0])
+        jaccard_classwise = {v: np.sum(jaccard_matrix[:, v])/(class_img_map[v]*dims) if class_img_map[v] != 0.0 else 0.0
                              for v in range(jaccard_matrix.shape[-1])}
         print("ver: {} - jaccard_classwise: {}".format(ver, jaccard_classwise))
         jaccard_overall = np.mean(
@@ -64,11 +75,11 @@ def jaccard(params, rng, x_patch, x, y, ver, save_img_to_folder, forward_fn, con
         print("jaccard_overall: {}".format(jaccard_overall))
     # Dice
     # dice = 2 * (TP) / ( (TP+FP)+(TP+FN) )
-    #TP = logits_red == y_red
+    # TP = logits_red == y_red
 
-    #print("np.unique(y_hat): {}".format(np.unique(y_hat)))
-    #print("np.unique(y): {}".format(np.unique(y)))
-    save_img_to_folder(config, x, y, logits, ver)
+    # print("np.unique(y_hat): {}".format(np.unique(y_hat)))
+    # print("np.unique(y): {}".format(np.unique(y)))
+    save_img_to_folder(config, x, y_ohe, logits, ver)
 
     return jaccard_overall
 
