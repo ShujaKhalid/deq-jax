674ed82165cff63f5b6945c25b4c87eff9eb9cd3
Shuja Khalid <shujakhalid@gmail.com>
2022-07-11 11:25:03 -0400

    Training now. somewhat working - minor fix for jaccard and dice

--- runner.sh
+++ runner.sh
@@ -3,13 +3,13 @@
 #DATASET_PATH='/home/skhalid/Documents/datalake/shakespeare.txt'
 # DATASET_PATH='/home/skhalid/Documents/datalake/cifar-10-python/cifar-10-batches-py'
 #rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/base.json"
-#rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/base.json"
+rm -rf /tmp/haiku-transformer/checkpoint_* && python -W ignore train.py --job_id "./jobs/base.json"
 
-jobs=$(ls -lrt ./jobs/vector/*.json | awk '{print $9}')
-jobs_dir=./jobs/vector/
-for job in ${jobs[@]}
-do
-    echo $job
-    sbatch ./_launch.sh $job
-    sleep 2
-done
+# jobs=$(ls -lrt ./jobs/vector/*.json | awk '{print $9}')
+# jobs_dir=./jobs/vector/
+# for job in ${jobs[@]}
+# do
+#     echo $job
+#     sbatch ./_launch.sh $job
+#     sleep 2
+# done
